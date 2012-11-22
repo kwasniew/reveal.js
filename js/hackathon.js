b127ff3448d48863a57243d5cf8476d86c357e9f
krzychukula <krzychukula@gmail.com>
2012-11-22 11:39:41 +0100

    back to top

--- js/hackathon.js
+++ js/hackathon.js
@@ -13,7 +13,7 @@ $(function () {
 						<p></p>
 					</section>
 					*/
-	function tmplArticle(article, category,  i){
+	function tmplArticle(article, category,  i, limit){
 		var articleView = $('<section>').append('<div class="parentCategory"><a href="#/3" class="">back to Shows</a></div>');
 			
 			var heading = $('<h2>').text(article.tittel);
@@ -22,21 +22,31 @@ $(function () {
 				var image = $('<img>').attr({src: article.media.w480});
 				articleView.append(image);
 			}
-			articleView.append('<div class="parentCategory"><a href="#/'+category+'/'+(i+2)+
+			articleView.append('<div class="jqModal hidden">'+article.description+'</div>');
+			if(i+1 < limit){
+				articleView.append('<div class="parentCategory"><a href="#/'+category+'/'+(i+2)+
 				'" class="">see next</a></div>');
+			}else{
+				articleView.append('<div class="parentCategory"><a href="#/'+category+
+				'" class="">back to top</a></div>');
+			}
+			
 		return articleView;
+	}
+
+	function appendArticles(list, name){
+		var articles = [];
+		var length = list.length;
+		list.forEach(function (article, i) {
+			articles.push(tmplArticle(article, showsColumn, i, length));
+		});
+		$(name).append(articles);
 	}
 
 	var showsColumn = 3;
 
 	if(ap.shows){
-
-		var articles = [];
-		ap.shows.forEach(function (article, i) {
-			articles.push(tmplArticle(article, showsColumn, i));
-		});
-		$('#shows').append(articles);
-
+		appendArticles(ap.shows, '#shows');
 	}
 
 
